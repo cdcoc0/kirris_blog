2926255b040228e47a0bc2d8b5e52915ae310ec2
Jiyeon Choi <jiyunee13@gmail.com>
2021-09-06 18:37:37 +0900

    read post

--- blog-server/src/main/java/kirris/blog/controller/PostsController.java
+++ blog-server/src/main/java/kirris/blog/controller/PostsController.java
@@ -1,15 +1,13 @@
 package kirris.blog.controller;
 
+import kirris.blog.domain.Posts;
 import kirris.blog.service.PostsService;
 import kirris.blog.domain.PostsRequestDto;
 import kirris.blog.domain.PostsResponseDto;
 import kirris.blog.repository.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,10 +25,26 @@ public class PostsController {
         //400 Bad request, 500 Internal Server Error
     }
 
+    @Transactional(readOnly = true)
     @GetMapping("/")
     public List<PostsResponseDto> list() {
         return postsRepository.findAll().stream()
                 .map(PostsResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @GetMapping("/{id}")
+    public PostsResponseDto read(@PathVariable("id") Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remove(@PathVariable("id") Long id) {
+        postsRepository.delete(id);
+        //status = 204; No Content
     }
 }
--- blog-server/src/main/java/kirris/blog/repository/PostsRepository.java
+++ blog-server/src/main/java/kirris/blog/repository/PostsRepository.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
@@ -23,9 +25,18 @@ public class PostsRepository {
     }
 
     public List<Posts> findAll() {
-        return em.createQuery("select p from Posts p order by p.id desc")
+        return em.createQuery("select p from Posts p order by p.id desc", Posts.class)
                 //.setFirstResult()
                 //.setMaxResults(9)
                 .getResultList();
+    }
+
+    public Optional<Posts> findById(Long id) {
+        Posts post =  em.find(Posts.class, id);
+        return Optional.ofNullable(post);
+    }
+
+    public void delete(Long id) {
+
     }
 }
